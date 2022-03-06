a57a9ea9fadd87a9f83266699f40d2d5bd4772bf
felipemunozca <analista.felipe.m@gmail.com>
2022-03-06 00:41:24 -0300

    codigo javascript para imprimir el grafico de pastel

--- assets/js/scriptSuperHero.js
+++ assets/js/scriptSuperHero.js
@@ -23,6 +23,9 @@ btnBuscarHero.addEventListener('click', (event) => {
 
                 //llamo a mis funciones
                 crearTarjeta(heroe);
+                crearGrafico(heroe);
+                limpiarInput();
+                $('.superheroe-section').show(1000);
             }
         });
 
@@ -63,3 +66,62 @@ const crearTarjeta = (heroeRecibido) => {
         <p class="card-text">Alianzas: ${alianza}</p>
     `);
 };
+
+
+const crearGrafico = (heroeRecibido) => {
+
+    let inteligencia = heroeRecibido.powerstats.intelligence;
+    let fuerza = heroeRecibido.powerstats.strength;
+    let velocidad = heroeRecibido.powerstats.speed;
+    let resistencia = heroeRecibido.powerstats.durability;
+    let poder = heroeRecibido.powerstats.power;
+    let combate = heroeRecibido.powerstats.combat;
+
+    //creo una condicion if para comparar si es alguno de los valores de powerstats viene vacio. Mostrara una alerta en pantalla.
+    if (inteligencia == "null" || fuerza == "null" || velocidad == "null" || resistencia == "null" || poder == "null" || combate == "null" ) {
+        alert('¡¡Error!! Uno o más valores del Super Heroe no se encuentran disponibles, así que el grafico puede que no se muestre.');
+    }
+
+    //codigo para imprimir el grafico.
+    let grafico = new CanvasJS.Chart("contenedorGrafico", {
+        animationEnabled: true,	
+        title:{
+            text: `Estadisticas de poder para ${heroeRecibido.name}`,
+        },
+        legend:{
+            cursor: "pointer",
+            itemclick: explodePie
+        },
+        data: [{
+            type: "pie",
+            showInLegend: true,
+            toolTipContent: "{name}: <strong>{y}%</strong>",
+            indexLabel: "{name} - {y}%",
+            dataPoints: [
+                //imprimo las respuestas directamente ya que no pude hacerlo con ciclo for.
+                { y: inteligencia, name: "Intelligence" },
+                { y: fuerza, name: "Strength" },
+                { y: velocidad, name: "Speed" },
+                { y: resistencia, name: "Durability" },
+                { y: poder, name: "Power" },
+                { y: combate, name: "Combat"}
+            ]
+        }]
+    });
+    grafico.render();
+};
+
+//codigo que es parte de canvasJS.
+function explodePie (e) {
+    if(typeof (e.dataSeries.dataPoints[e.dataPointIndex].exploded) === "undefined" || !e.dataSeries.dataPoints[e.dataPointIndex].exploded) {
+        e.dataSeries.dataPoints[e.dataPointIndex].exploded = true;
+    } else {
+        e.dataSeries.dataPoints[e.dataPointIndex].exploded = false;
+    }
+}
+
+
+//funcion para limpiar el input cuando el codigo se ejecute correctamente.
+function limpiarInput(){
+    document.getElementById("inputNumeroHero").value = "";
+}
